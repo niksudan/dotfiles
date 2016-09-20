bde113d7744ff5c7011af5a60814453292e7c5cc
Nik Sudan <niksudan@gmail.com>
2016-09-20 12:05:23 +0100

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -1,27 +1,18 @@
 # Detailed file listing
-alias l='ls -al'
+alias l='ls -alh'
+
+# Better directory listing
+alias ls='ls -aGpF'
 
 # Reload shell
-alias rl='source ~/.bash_aliases && clear && echo "Shell successfully reloaded"'
-alias reload='rl'
+alias rl='source ~/.bash_aliases && echo "Reloaded aliases"'
 
 # Quick alias editing
 alias a='vim ~/.bash_aliases'
 
-# Better directory listing
-alias ls='ls -G'
-
-# Directory listing with sizes
-alias size='echo "Diskspace report:" && df -P -kHl && echo "Calculating current directory content sizes..." && du -hs *'
-
-# Filesize
-alias fs='stat -f \"%z bytes\"'
-
-# Jump to git repos
-alias gits='cd ~/Git && clear &&  echo "Choose a repo, or start a new one!" && ls'
-
 # Vim
 alias v=vim
+alias vi=vim
 
 # Hosts
 alias hosts='sudo vim /etc/hosts'
@@ -29,14 +20,29 @@ alias hosts='sudo vim /etc/hosts'
 # SSH Keys
 alias sshs='cd ~/.ssh'
 
-# Clear console
-alias c='clear && echo "Bang and the dirt is gone!"'
-
-# Easier navigation: .., ..., ~ and -
+# Easier navigation
 alias ..="cd .."
-alias cd..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../.."
 alias .....="cd ../../../.."
-alias ~="cd ~" # `cd` is probably faster to type though
-alias -- -="cd -"
+
+# Jump to repos
+alias gits='cd ~/Git'
+
+# Generate new ssh connection
+conn() {
+~/Scripts/conn "$@"
+}
+
+# Composer
+alias composer='php /usr/local/bin/composer.phar'
+
+# Find and replace
+replace() {
+~/Scripts/replace "$@"
+}
+
+# Copy to clipboard
+copy() {
+cat $1 | pbcopy
+}
