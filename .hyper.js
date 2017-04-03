a7448c096451061fe39d03d4085e237956f241a1
Nik Sudan <niksudan@gmail.com>
2017-04-03 15:53:04 +0100

    Create .hyper.js

---
+++ .hyper.js
@@ -0,0 +1,40 @@
+module.exports = {
+  config: {
+    fontFamily: 'FiraCode-Regular, monospace',
+    termCSS: 'x-screen x-row { font-variant-ligatures: contextual; }',
+    padding: '12px 14px',
+    backgroundColor: 'rgba(40, 42, 54, 0.5)',
+    colors: {
+      black: '#000000',
+      red: '#D62341',
+      green: '#9ECE58',
+      yellow: '#FAED70',
+      blue: '#396FE2',
+      magenta: '#BB80B3',
+      cyan: '#2DDAFD',
+      white: '#d0d0d0',
+      lightBlack: 'rgba(255, 255, 255, 0.2)',
+      lightRed: '#FF5370',
+      lightGreen: '#C3E88D',
+      lightYellow: '#FFCB6B',
+      lightBlue: '#82AAFF',
+      lightMagenta: '#C792EA',
+      lightCyan: '#89DDFF',
+      lightWhite: '#ffffff'
+    },
+    shell: '/bin/zsh',
+    shellArgs: ['--login'],
+    env: {},
+    bell: 'SOUND',
+    bellSoundURL: 'https://dl.dropboxusercontent.com/u/53826881/Notification.wav'
+  },
+  plugins: [
+    'hyper-hide-title',
+    'hypercwd',
+    'hyper-statusline',
+  ],
+  localPlugins: [
+    './hyper-vibrancy',
+    './hyper-snazzy'
+  ]
+};
