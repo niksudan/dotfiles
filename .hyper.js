7a48bf8cc714b38d5a2951ea37bc29fe434970a3
Nik Sudan <niksudan@gmail.com>
2019-09-03 09:45:33 +0100

    Update .hyper.js

--- .hyper.js
+++ .hyper.js
@@ -5,10 +5,14 @@ module.exports = {
     fontWeight: 400,
     termCSS: 'x-screen x-row { font-variant-ligatures: contextual; }',
     padding: '12px 14px',
-    backgroundColor: 'rgba(20, 20, 20)',
     shell: '/bin/zsh',
     shellArgs: ['--login'],
     env: {},
   },
-  plugins: ['hyper-hide-title', 'hypercwd', 'hyper-statusline'],
+  plugins: [
+    'hyper-hide-title',
+    'hypercwd',
+    'hyper-statusline',
+    'hyper-one-dark',
+  ],
 };
